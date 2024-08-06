26ebeb779666c4eb9a82864603e15f47cb2cbf55
widiantoko <dodiwidiantoko@gmail.com>
2024-08-06 18:00:07 +0700

    tt

--- knp_error.py
+++ knp_error.py
@@ -47,7 +47,7 @@ while i < len(x_gab):
         opacity=1,
         colorscale=pick_color[i],
         hovertext=hvr_txt[i],
-        hoverinfo="text",
+        #hoverinfo="",
         hovertemplate="Batch: %{value}, Produk: %{hovertext}"
 
 

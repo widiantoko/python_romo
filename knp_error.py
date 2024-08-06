451f191bf554d1f0b60a81a7c568d849cbd40f30
widiantoko <dodiwidiantoko@gmail.com>
2024-08-06 16:13:36 +0700

    ll

--- knp_error.py
+++ knp_error.py
@@ -45,7 +45,6 @@ while i < len(x_gab):
         z=z_gab[i],
         value=v_gab[i],
         showscale=False,
-        hoverlabel=False,
         opacity=1,
         colorscale=pick_color[i],
         hovertext=hvr_txt[i]

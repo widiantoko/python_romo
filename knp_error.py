0243146365a2ee1e058691763038ee4c0aadb0ce
widiantoko <dodiwidiantoko@gmail.com>
2024-08-06 16:33:44 +0700

    yyu

--- knp_error.py
+++ knp_error.py
@@ -47,8 +47,7 @@ while i < len(x_gab):
         showscale=False,
         opacity=1,
         colorscale=pick_color[i],
-        hovertemplate=hvr_txt[i]
-    
+        hovertext=hvr_txt[i]
        ))
     i += 1  # Update kondisi iterasi
 

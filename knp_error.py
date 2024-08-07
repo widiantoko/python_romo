07a3e6f43e04c5b73b498e32bcbc43ff16e438a0
widiantoko <dodiwidiantoko@gmail.com>
2024-08-07 18:39:50 +0700

    yy

--- knp_error.py
+++ knp_error.py
@@ -128,7 +128,7 @@ fig_test_new.update_layout(
 
 
 
-fig_test_new.update_layout(autosize=True, width=1000,height=1000, margin=dict(t=0, r=0, l=0, b=0,),)
+fig_test_new.update_layout(autosize=True, width=1800,height=600, margin=dict(t=0, r=0, l=0, b=0,),)
 
 fig_test_new.update_layout(scene = dict(xaxis = dict(title='Blok AA',tickangle=0, showticklabels=False),
                     yaxis = dict(title='Depth', showticklabels=False),

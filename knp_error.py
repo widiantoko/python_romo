8cfdc55ffc65fa2849bb7e1ce4be5e51039d360a
widiantoko <dodiwidiantoko@gmail.com>
2024-08-08 12:55:12 +0700

    test

--- knp_error.py
+++ knp_error.py
@@ -10,8 +10,10 @@ st.set_page_config(
 
 
 
-storage=pd.read_csv('test_xwh.csv', delimiter=';',decimal=',', dtype={'Aisle':str}) 
-storage['posisi']=storage["Zona"].apply(str)+"."+storage["Aisle"].apply(str)
+#storage=pd.read_csv('test_xwh.csv', delimiter=';',decimal=',', dtype={'Aisle':str}) 
+storage=pd.read_csv('test_xwh_new.csv', delimiter=';',decimal=',') 
+
+#storage['posisi']=storage["Zona"].apply(str)+"."+storage["Aisle"].apply(str)
 storage['color'] = storage['Nm_Brg'].apply(lambda x: 'Plasma' if x == '0052 - WANKAI' else 'Blues')
 
 
@@ -22,7 +24,9 @@ z_gab=storage[['z1','z2','z3','z4','z5','z6','z7','z8']].values.tolist()
 v_gab=storage[['No_Batch','No_Batch','No_Batch','No_Batch','No_Batch','No_Batch','No_Batch','No_Batch']].values.tolist()
 hvr_txt=storage[['Nm_Brg','Nm_Brg','Nm_Brg','Nm_Brg','Nm_Brg','Nm_Brg','Nm_Brg','Nm_Brg' ]].values.tolist()
 pick_color = storage['color'].tolist()
-lokasi = storage[['posisi','posisi','posisi','posisi','posisi','posisi','posisi','posisi']].values.tolist()
+#lokasi = storage[['posisi','posisi','posisi','posisi','posisi','posisi','posisi','posisi']].values.tolist()
+lokasi = storage[['Posisi','Posisi','Posisi','Posisi','Posisi','Posisi','Posisi','Posisi']].values.tolist()
+
 
 #st.text(storage['posisi'])
 

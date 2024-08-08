52a7acebdb79f7d6cfb6684c0ff9360580d0cc1d
widiantoko <dodiwidiantoko@gmail.com>
2024-08-08 12:51:01 +0700

    nn

--- knp_error.py
+++ knp_error.py
@@ -7,67 +7,6 @@ st.set_page_config(
     page_title="Storage_Warehouse",
     layout="wide",)
 
-storage_new=pd.read_csv('uji_coba_baru.csv', delimiter=';',decimal=',', dtype={'aisle':str}) 
-
-
-
-
-storage_new['color'] = storage_new['nm_brg'].apply(lambda x: 'balance' if x == '0052 - WANKAI' else 'Plasma')
-
-
-#for a in storage['position']:
-x_gab_new=storage_new[['x1','x2','x3','x4','x5','x6','x7','x8']].values.tolist()
-y_gab_new=storage_new[['y1','y2','y3','y4','y5','y6','y7','y8']].values.tolist()
-z_gab_new=storage_new[['z1','z2','z3','z4','z5','z6','z7','z8']].values.tolist()
-v_gab_new=storage_new[['no_batch','no_batch','no_batch','no_batch','no_batch','no_batch','no_batch','no_batch']].values.tolist()
-hvr_txt_new=storage_new[['nm_brg','nm_brg','nm_brg','nm_brg','nm_brg','nm_brg','nm_brg','nm_brg' ]].values.tolist()
-pick_color_new = storage_new['color'].tolist()
-lokasi_new = storage_new[['posisi','posisi','posisi','posisi','posisi','posisi','posisi','posisi']].values.tolist()
-
-#st.text(storage['posisi'])
-
-fig_test_new = go.Figure()
-i = 0
-while i < len(x_gab_new):
-    fig_test_new.add_trace(go.Isosurface(
-        x=x_gab_new[i],
-        y=y_gab_new[i],
-        z=z_gab_new[i],
-        value=v_gab_new[i],
-        showscale=False,
-        opacity=1,
-        colorscale=pick_color_new[i],
-        text=lokasi_new[i],
-        hovertext=hvr_txt_new[i],
-        hovertemplate=
-            "<b>Lokasi:</b> %{text}"
-            "<br><b>Produk:</b> %{hovertext}" 
-            "<br><b>Batch:</b> %{value:d3-format} <extra></extra>"
-       
-
-
-        
-       ))
-    i += 1  # Update kondisi iterasi
-
- 
-fig_test_new.update_layout(
-    hoverlabel=dict(
-        bgcolor="white",
-        font_size=12,
-        font_family="Arial"),)
-
-
-fig_test_new.update_layout(autosize=False, width=3000,height=600, margin=dict(t=20, r=0, l=0, b=20,),)
-
-fig_test_new.update_layout(scene = dict(xaxis = dict(title='Blok AA',tickangle=45, showticklabels=False),
-                    yaxis = dict(title='Depth', showticklabels=False),
-                    zaxis = dict(title='Level', showticklabels=False), ))
-
-
-#st.plotly_chart(fig_test_new)
-
-
 
 
 

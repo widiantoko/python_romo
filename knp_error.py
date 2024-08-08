d0492007ccc7304ba91eb5ebbb5a353c7341e8e8
widiantoko <dodiwidiantoko@gmail.com>
2024-08-08 19:56:42 +0700

    yy

--- knp_error.py
+++ knp_error.py
@@ -65,7 +65,7 @@ fig_test.update_layout(
 
 camera = dict(
     #up=dict(x=0, y=0, z=1),
-#    center=dict(x=0, y=1, z=0),
+   center=dict(x=2, y=0, z=0),
     eye=dict(x=2, y=2, z=1.2))
 
 fig_test.update_layout(scene_camera=camera)

ca70873fd915fdc9d6bcbb04282fca93839a4b78
widiantoko <dodiwidiantoko@gmail.com>
2024-08-08 08:02:55 +0700

    yyy

--- knp_error.py
+++ knp_error.py
@@ -73,7 +73,7 @@ fig_test_new.update_layout(scene = dict(xaxis = dict(title='Blok AA',tickangle=4
 
 storage=pd.read_csv('test_xwh.csv', delimiter=';',decimal=',', dtype={'Aisle':str}) 
 storage['posisi']=storage["Zona"].apply(str)+"."+storage["Aisle"].apply(str)
-storage['color'] = storage['Nm_Brg'].apply(lambda x: 'balance' if x == '0052 - WANKAI' else 'Plasma')
+storage['color'] = storage['Nm_Brg'].apply(lambda x: 'Balance' if x == '0052 - WANKAI' else 'Green')
 
 
 #for a in storage['position']:

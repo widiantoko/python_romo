4ebe84121649f32ddff83bcad1c2571ec40fce4f
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 18:54:37 +0700

    tst

--- knp_error.py
+++ knp_error.py
@@ -123,7 +123,9 @@ BW_8 = {6.95,	6.00,	6.95,	6.00,	6.95,	6.00,	6.95,	6.00}
 AT_8 = {6.95,	6.00,	6.95,	6.00,	6.95,	6.00,	6.95,	6.00}
 
 
-result_new[['y1'],['y2'],['y3'],['y4'],['y5'],['y6'],['y7'],['y8']] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
+#result_new[['y1'],['y2'],['y3'],['y4'],['y5'],['y6'],['y7'],['y8']] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
+
+result_new['y1','y2','y3','y4','y5','y6','y7','y8'] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
 
 
 result_new['test'] = result_new['seq'].apply(lambda x: 'genap' if x %2 ==  0 else 'ganjil')

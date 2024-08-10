bb974937f6fbc85a55115a9d7f5fe33363b93350
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 20:50:10 +0700

    uuu

--- knp_error.py
+++ knp_error.py
@@ -108,8 +108,8 @@ result_new['x8']= result_new['bayint']-0.05
 
 
 #BW_1A = ['y1':0.95, 'y2':0.00, 'y3':0.95,	'y4':0.00,	'y5':0.95,	'y6':0.00,	'y7':0.95,	'y8':0.00]
-BW_1A = (0.95,0.00,0.95,0.00,0.95,0.00,0.95,0.00)
-AT_1 = (0.95,	0.00,	0.95,	0.00,	0.95,	0.00,	0.95,	0.00)
+BW_1A = [0.95,0.00,0.95,0.00,0.95,0.00,0.95,0.00]
+AT_1 = [0.95,	0.00,	0.95,	0.00,	0.95,	0.00,	0.95,	0.00]
 BW_2 = {1.95,	1.00,	1.95,	1.00,	1.95,	1.00,	1.95,	1.00}
 AT_3 = {1.95,	1.00,	1.95,	1.00,	1.95,	1.00,	1.95,	1.00}
 BW_4 = {2.95,	2.00,	2.95,	2.00,	2.95,	2.00,	2.95,	2.00}
@@ -127,7 +127,7 @@ AT_8 = {6.95,	6.00,	6.95,	6.00,	6.95,	6.00,	6.95,	6.00}
 #result_new['y1'],['y2'],['y3'],['y4'],['y5'],['y6'],['y7'],['y8'] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
 #result_new[['y1','y2','y3','y4','y5','y6','y7','y8']] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
 result_new['gab'] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else AT_1)
-result_new[['y1','y2','y3','y4','y5','y6','y7','y8']] = result_new['gab'].str.split(' ', expand=True)
+#result_new[['y1','y2','y3','y4','y5','y6','y7','y8']] = result_new['gab'].str.split(' ', expand=True)
 
 #df[['First Name', 'Last Name']] = df['Name'].str.split(' ', expand=True)
 

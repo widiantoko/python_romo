bfe6105d8e5317c2350097359b9ac59f762fe46d
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 15:09:55 +0700

    yy

--- knp_error.py
+++ knp_error.py
@@ -97,14 +97,14 @@ ids=result_new['loc_new'].drop_duplicates()
 
 result_new['no_urut'] = (result_new['loc_new'] != result_new['loc_new'].shift(1)).cumsum()
 
-#result_new['count'] = result_new.groupby('loc_new')['loc_new'].transform('count')
+result_new['count'] = result_new.groupby('loc_new')['loc_new'].transform('count')
 #per_loc=result_new['count'].drop_duplicates()
 
 st.dataframe(result_new.head(30))
 
 result_new['seq'] = ""
 for i in range(0, len(result_new)):
-    result_new.loc[i, 'seq'] = 1 if result_new.loc[i, 'no_urut'] == 0 else  result_new.loc[i - 1, 'seq'] + 1
+    result_new.loc[i, 'seq'] = 1 if result_new.loc[i, 'count'] == 0 else  result_new.loc[i - 1, 'seq'] + 1
 
 
 #seq_list = []

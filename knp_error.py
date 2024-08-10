9876b69647ffbdc6b6d7eb569a34a054e9201313
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 17:01:26 +0700

    ss

--- knp_error.py
+++ knp_error.py
@@ -106,8 +106,6 @@ result_new['x8']= result_new['bayint']-0.05
 
 
 for num in result_new['qty_bag']:
-
-    # checking condition
     if (num % 2) == 0:
         result_new['test']='genap'
     else:

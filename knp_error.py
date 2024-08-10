a2c6c43ec60b5f0d3592660b9adf3543cef8f06e
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 16:50:50 +0700

    test

--- knp_error.py
+++ knp_error.py
@@ -105,10 +105,10 @@ result_new['x7']= result_new['bayint']-0.05
 result_new['x8']= result_new['bayint']-0.05
 
 
-for num in result_new['seq']:
-    if num % 2 == 0:
+
+if result_new['seq'] % 2 == 0:
         result_new['test']='genap'
-    else:
+else:
         result_new['test']='ganjil'
 
 

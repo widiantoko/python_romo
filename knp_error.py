8e9234092e33870827625d6b99224fa17b1d8af8
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 20:36:23 +0700

    ttr

--- knp_error.py
+++ knp_error.py
@@ -126,6 +126,9 @@ AT_8 = {6.95,	6.00,	6.95,	6.00,	6.95,	6.00,	6.95,	6.00}
 
 #result_new['y1'],['y2'],['y3'],['y4'],['y5'],['y6'],['y7'],['y8'] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
 #result_new[['y1','y2','y3','y4','y5','y6','y7','y8']] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
+result_new[['gab']] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
+
+#df[['First Name', 'Last Name']] = df['Name'].str.split(' ', expand=True)
 
 
 result_new['test'] = result_new['seq'].apply(lambda x: 'genap' if x %2 ==  0 else 'ganjil')

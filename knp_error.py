b1b3042477ffbdf3fa29a3dd9bfc0cb41a1e74c5
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 21:13:32 +0700

    ysy

--- knp_error.py
+++ knp_error.py
@@ -127,7 +127,7 @@ AT_8 = {6.95,	6.00,	6.95,	6.00,	6.95,	6.00,	6.95,	6.00}
 #result_new['y1'],['y2'],['y3'],['y4'],['y5'],['y6'],['y7'],['y8'] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
 #result_new[['y1','y2','y3','y4','y5','y6','y7','y8']] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else None)
 result_new['gab'] = result_new['posisi'].apply(lambda x:  BW_1A if x=='BW_1' else BW_2)
-result_new['y1','y2','y3','y4','y5','y6','y7','y8']= result_new['gab'].str.split(',', expand=True)
+result_new[[']y1','y2','y3','y4','y5','y6','y7','y8']]= result_new['gab'].str.split(',', expand=True)
 
 
 

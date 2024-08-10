c0d424865893148acd9159bade11f2740e98c40e
widiantoko <dodiwidiantoko@gmail.com>
2024-08-10 13:58:44 +0700

    uiu

--- knp_error.py
+++ knp_error.py
@@ -96,6 +96,8 @@ ids=result_new['loc_new'].drop_duplicates()
 st.text(ids)
 
 result_new['no_urut'] = (result_new['loc_new'] != result_new['loc_new'].shift(1)).cumsum()
+result_new['count'] = result_new.groupby('loc_new')['loc_new'].transform('count')
+
 #result_new.loc[result_new.loc_new.isin(ids), 'no_urut_br'] += 1
 
 
